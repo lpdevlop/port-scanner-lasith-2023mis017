fb2542d2761d33fb6f7573a72885a59955b9ae75
lasithpalihawadana <lasithpalihawadana@gmail.com>
2025-08-24 17:00:13 +0530

    Add Packet Capture

---
+++ packet-capture.py
@@ -0,0 +1,38 @@
+from scapy.all import sniff
+from datetime import datetime
+import socket
+import uuid
+
+from scapy.layers.inet import TCP, UDP, IP
+
+
+def get_local_ip():
+    return socket.gethostbyname(socket.gethostname())
+
+def get_mac_address():
+    mac = uuid.getnode()
+    mac_addr = ':'.join(['{:02x}'.format((mac >> ele) & 0xff)
+                         for ele in range(40, -1, -8)])
+    return mac_addr
+
+def process_packet(packet):
+    time = datetime.now().strftime("%Y/%m/%d %H:%M")
+    ip = get_local_ip()
+    mac = get_mac_address()
+
+    description = ""
+
+    if packet.haslayer(TCP):
+        description = f"TCP Packet. Port: {packet[TCP].dport}"
+    elif packet.haslayer(UDP):
+        description = f"UDP Packet. Port: {packet[UDP].dport}"
+    elif packet.haslayer(IP):
+        description = f"IP Packet. Protocol: {packet[IP].proto}"
+    else:
+        description = "Other Packet Type"
+
+    print(f"Time: {time}, IP: {ip}, Mac Address: {mac},\nRequest: {description}\n")
+
+
+print("📡 Starting packet capture (Press Ctrl+C to stop)...")
+sniff(prn=process_packet, store=0)
